70e112fc038839dfb5663d679336c1eb5f209c95
marinalopez2110 <marinalopez2110@gmail.com>
2020-01-06 12:41:52 -0500

    Test GitHub

--- Portraits-Shiny/ui.R
+++ Portraits-Shiny/ui.R
@@ -1,6 +1,7 @@
 shinyServer (
   pageWithSidebar(
     headerPanel("Portraits Climatiques du Québec - MFFP"),
+    #Test git
     
     sidebarPanel(
       selectInput("Variable", "Séléctionez la variable climatique:",

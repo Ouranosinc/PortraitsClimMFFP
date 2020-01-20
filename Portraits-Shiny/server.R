cbfcf98be377c765c6251a59994010943d87df0a
marinalopez2110 <marinalopez2110@gmail.com>
2020-01-20 15:05:47 -0500

    Trying to get conditional pannel and reative working

--- Portraits-Shiny/server.R
+++ Portraits-Shiny/server.R
@@ -4,57 +4,57 @@ shinyServer(
     
     
     
-    #if (input$Echelle == "Régions forestières") {
-        # output$myImage <- renderImage({
-        #   filename <- normalizePath(file.path('./www', paste(input$RegForest,"Moyenne_Annuel_Hist_Elevees.png", sep='')))
-        #   list(src = filename, alt = paste("Echele Spatiale ", input$RegForest))
-        #   }, deleteFile = FALSE)
-        # 
-        #  output$myImage2 <- renderImage({
-        #   filename <- normalizePath(file.path('./www', paste(input$RegForest,"Moyenne_Annuel_2050_",input$Scenario,".png", sep='')))
-        #   list(src = filename, alt = paste("Echele Spatiale ", input$RegForest))
-        #  }, deleteFile = FALSE)
-        # 
-        #  output$myImage3 <- renderImage({
-        #   filename <- normalizePath(file.path('./www', paste(input$RegForest,"Moyenne_Annuel_Hist_Elevees.png", sep='')))
-        #   list(src = filename, alt = paste("Echele Spatiale ", input$RegForest))
-        # }, deleteFile = FALSE)
-        # 
-        #  output$myImage4 <- renderImage({
-        #   filename <- normalizePath(file.path('./www', paste(input$RegForest,"Moyenne_Annuel_2080_",input$Scenario,".png", sep='')))
-        #   list(src = filename,alt = paste("Echele Spatiale ", input$RegForest))
-        # }, deleteFile = FALSE)
-    #}
+    if (input$Echelle == "Régions forestières") {
+    output$myImage <- renderImage({
+      filename <- normalizePath(file.path('./www', paste(input$RegForest,"Moyenne_Annuel_Hist_Elevees.png", sep='')))
+      list(src = filename, alt = paste("Echele Spatiale ", input$RegForest))
+      }, deleteFile = FALSE)
+
+     output$myImage2 <- renderImage({
+      filename <- normalizePath(file.path('./www', paste(input$RegForest,"Moyenne_Annuel_2050_",input$Scenario,".png", sep='')))
+      list(src = filename, alt = paste("Echele Spatiale ", input$RegForest))
+     }, deleteFile = FALSE)
+
+     output$myImage3 <- renderImage({
+      filename <- normalizePath(file.path('./www', paste(input$RegForest,"Moyenne_Annuel_Hist_Elevees.png", sep='')))
+      list(src = filename, alt = paste("Echele Spatiale ", input$RegForest))
+    }, deleteFile = FALSE)
+
+     output$myImage4 <- renderImage({
+      filename <- normalizePath(file.path('./www', paste(input$RegForest,"Moyenne_Annuel_2080_",input$Scenario,".png", sep='')))
+      list(src = filename,alt = paste("Echele Spatiale ", input$RegForest))
+    }, deleteFile = FALSE)
+    }
     # else if (input$Echelle == "Territoires guides") {
-      output$myImage <- renderImage({
-        filename <- normalizePath(file.path('./www', paste(input$Territoires,"Moyenne_Annuel_Hist_Moderees.png", sep='')))
-        list(src = filename, alt = paste("Echele Spatiale ", input$Territoires))
-      }, deleteFile = FALSE)
-
-      output$myImage2 <- renderImage({
-        filename <- normalizePath(file.path('./www', paste(input$Territoires,"Moyenne_Annuel_2050_",input$Scenario,".png", sep='')))
-        list(src = filename, alt = paste("Echele Spatiale ", input$Territoires))
-      }, deleteFile = FALSE)
-
-      output$myImage3 <- renderImage({
-        filename <- normalizePath(file.path('./www', paste(input$Territoires,"Moyenne_Annuel_Hist_Moderees.png", sep='')))
-        list(src = filename, alt = paste("Echele Spatiale ", input$Territoires))
-      }, deleteFile = FALSE)
-
-      output$myImage4 <- renderImage({
-        filename <- normalizePath(file.path('./www', paste(input$Territoires,"Moyenne_Annuel_2080_",input$Scenario,".png", sep='')))
-        list(src = filename,alt = paste("Echele Spatiale ", input$Territoires))
-      }, deleteFile = FALSE)
+    #   output$myImage <- renderImage({
+    #     filename <- normalizePath(file.path('./www', paste(input$Territoires,"Moyenne_Annuel_Hist_Moderees.png", sep='')))
+    #     list(src = filename, alt = paste("Echele Spatiale ", input$Territoires))
+    #   }, deleteFile = FALSE)
+    # 
+    #   output$myImage2 <- renderImage({
+    #     filename <- normalizePath(file.path('./www', paste(input$Territoires,"Moyenne_Annuel_2050_",input$Scenario,".png", sep='')))
+    #     list(src = filename, alt = paste("Echele Spatiale ", input$Territoires))
+    #   }, deleteFile = FALSE)
+    # 
+    #   output$myImage3 <- renderImage({
+    #     filename <- normalizePath(file.path('./www', paste(input$Territoires,"Moyenne_Annuel_Hist_Moderees.png", sep='')))
+    #     list(src = filename, alt = paste("Echele Spatiale ", input$Territoires))
+    #   }, deleteFile = FALSE)
+    # 
+    #   output$myImage4 <- renderImage({
+    #     filename <- normalizePath(file.path('./www', paste(input$Territoires,"Moyenne_Annuel_2080_",input$Scenario,".png", sep='')))
+    #     list(src = filename,alt = paste("Echele Spatiale ", input$Territoires))
+    #   }, deleteFile = FALSE)
     # } 
     
   
 ### Download files 
-  output$downloadData <- downloadHandler(
-    filename = "Moyenne.csv",
-    content = function(file) { 
-      file.copy("Moyenne.csv",file)
-    }
-    )
+  # output$downloadData <- downloadHandler(
+  #   filename = "Moyenne.csv",
+  #   content = function(file) { 
+  #     file.copy("Moyenne.csv",file)
+  #   }
+  #   )
   
   # output$downloadData2 <- downloadHandler(
   #   filename = "Moyenne.csv",

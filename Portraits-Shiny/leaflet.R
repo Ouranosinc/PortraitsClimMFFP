7e5ec31163e103dffdb96ef17df4126a909e4a63
Marina Lopez <marinalopez2110@gmail.com>
2020-01-29 13:04:04 -0500

    Update leaflet.R
    Added a 3rd  region and legend. Played with colours.

--- Portraits-Shiny/leaflet.R
+++ Portraits-Shiny/leaflet.R
@@ -3,9 +3,15 @@ library(leaflet)
 library(shiny)
 library(sp)
 
-TG  <- geojsonio::geojson_read("C:\\Users\\mlopez\\Documents\\GitHub\\SuperZip\\www\\TG.json", what = "sp")
-OneA <- geojsonio::geojson_read("C:\\Users\\mlopez\\Documents\\GitHub\\SuperZip\\www\\OneA.json", what = "sp")
-TwoC <- geojsonio::geojson_read("C:\\Users\\mlopez\\Documents\\GitHub\\SuperZip\\www\\TwoC.json", what = "sp")
+#TG  <- geojsonio::geojson_read("C:\\Users\\mlopez\\Documents\\GitHub\\SuperZip\\www\\TG.json", what = "sp") # TELUQ
+#OneA <- geojsonio::geojson_read("C:\\Users\\mlopez\\Documents\\GitHub\\SuperZip\\www\\OneA.json", what = "sp") #TELUQ
+#TwoC <- geojsonio::geojson_read("C:\\Users\\mlopez\\Documents\\GitHub\\SuperZip\\www\\TwoC.json", what = "sp") #TELUQ
+
+TG  <- geojsonio::geojson_read("C:\\Users\\marlop1\\Documents\\GitHub\\SuperZip\\www\\TG.json", what = "sp") # DesktopOuranos
+OneA <- geojsonio::geojson_read("C:\\Users\\marlop1\\Documents\\GitHub\\SuperZip\\www\\OneA.json", what = "sp") #DesktopOuranos
+TwoC <- geojsonio::geojson_read("C:\\Users\\marlop1\\Documents\\GitHub\\SuperZip\\www\\TwoC.json", what = "sp") #DesktopOuranos
+ThreeD <- geojsonio::geojson_read("C:\\Users\\marlop1\\Documents\\GitHub\\SuperZip\\www\\ThreeD.json", what = "sp") #DesktopOuranos <- geojsonio::geojson_read("C:\\Users\\marlop1\\Documents\\GitHub\\SuperZip\\www\\TwoC.json", what = "sp") #DesktopOuranos
+
 class(TG)
 names(TG)
 
@@ -15,22 +21,36 @@ names(OneA)
 class(TwoC)
 names(TwoC)
 
-#SpP = SpatialPolygons(list(OneA,TwoC), 1:2)
-pal1a <- colorNumeric("RdYlBu", domain = OneA$tg_mean)
-pal2c <- colorNumeric("RdYlBu", domain = TwoC$tg_mean)
+bins <- c(14, 12, 10, 8, 6, 4, 2, 0, -2, -4, -5)
+
+# colorNumeric(c("red", "green", "blue"), 1:10)
+# 
+# pal <- colorBin("RdYlBu", domain = TG$tg_mean, bins = bins)
+# pal1a <- colorBin("RdYlBu", domain = OneA$tg_mean, bins = bins)
+# pal2c <- colorBin("RdYlBu", domain = TwoC$tg_mean, bins = bins)
+# pal3d <- colorBin("RdYlBu", domain = ThreeD$tg_mean, bins = bins)
+
+pal <- colorNumeric(c("blue", "yellow", "red"), domain = TG$tg_mean, -5:14)
+pal1a <- colorNumeric(c("blue", "yellow", "red"), domain = OneA$tg_mean, -5:14)
+pal2c <- colorNumeric(c("blue", "yellow", "red"), domain = TG$tg_mean, -5:14)
+pal3d <- colorNumeric(c("blue", "yellow", "red"), domain = ThreeD$tg_mean, -5:14)
 
 labels <- sprintf(
-  "<strong>%s</strong><br/>%g Â°C",
+  "<strong>%s</strong><br/>%g °C",
   TGT$TER_GUIDE, TG$tg_mean
 ) %>% lapply(htmltools::HTML)
 
 labels1 <- sprintf(
-  "<strong>%s</strong><br/>%g Â°C",
+  "<strong>%s</strong><br/>%g °C",
   OneA$TER_GUIDE, OneA$tg_mean
 ) %>% lapply(htmltools::HTML)
 labels2 <- sprintf(
-  "<strong>%s</strong><br/>%g Â°C",
+  "<strong>%s</strong><br/>%g °C",
   TwoC$TER_GUIDE, TwoC$tg_mean
+) %>% lapply(htmltools::HTML)
+labels3 <- sprintf(
+  "<strong>%s</strong><br/>%g °C",
+  ThreeD$TER_GUIDE, ThreeD$tg_mean
 ) %>% lapply(htmltools::HTML)
 
 m <- leaflet(OneA) %>%
@@ -43,7 +63,7 @@ m <- leaflet(OneA) %>%
     opacity = 1,
     color = "black",
     dashArray = "3",
-    fillOpacity = 0.7,
+    fillOpacity = 0.8,
     label = labels1,
     labelOptions = labelOptions(
       style = list("font-weight" = "normal", padding = "3px 8px"),
@@ -61,26 +81,33 @@ m <- leaflet(OneA) %>%
       weight = 2,
       color = "#666",
       dashArray = "",
-      fillOpacity = 0.7,
+      fillOpacity = 0.8,
       bringToFront = TRUE),
     label = labels2,
     labelOptions = labelOptions(
       style = list("font-weight" = "normal", padding = "3px 8px"),
       textsize = "15px",
-      direction = "auto"))
-
-
-m <- leaflet(OneA) %>%
-  setView(lat = 45.6, lng= -70.5, zoom = 7) %>%
-  addTiles()  %>%
+      direction = "auto"))%>%
   addPolygons (
-    fillColor = pal2c(TwoC$tg_mean),
-    data =  TwoC,
+    fillColor = pal3d(ThreeD$tg_mean),
+    data =  ThreeD,
     weight = 1,
     opacity = 1,
-    color = "blue",
+    color = "red",
     dashArray = "3",
-    fillOpacity = 0.7)
-
+    fillOpacity = 0.7,
+    highlight = highlightOptions(
+      weight = 2,
+      color = "#666",
+      dashArray = "",
+      fillOpacity = 0.8,
+      bringToFront = TRUE),
+    label = labels3,
+    labelOptions = labelOptions(
+      style = list("font-weight" = "normal", padding = "3px 8px"),
+      textsize = "15px",
+      direction = "auto"))%>% 
+  addLegend(pal = pal, values = ~TG$tg_mean, opacity = 0.7, title = NULL,
+                                        position = "bottomright")
 
 m
